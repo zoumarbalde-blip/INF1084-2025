76d905bb659832f410463232a6c7f478e2ffcecc
brobert <brobert@valiha.com>
2025-09-09 18:18:05 -0400

    .

--- .scripts/students.sh
+++ .scripts/students.sh
@@ -1,23 +1,73 @@
-|Image|Nom|Nom d’utilisateur
-ID défini par l’org.|Rôle|Date de l’accès le plus récent
-| Afficher le profil de Guiderian Taylor Ngue Ngan Kenfack|Ngue Ngan Kenfack, Guiderian Taylor|300138205|300138205|Student|sept. 7 2025 21 h 49
-| Afficher le profil de Djaber Benyezza|Benyezza, Djaber  Djaber Benyezza est en ligne|300146667|300146667|Student|sept. 9 2025 00 h 37
-| Afficher le profil de Boualem Belbessai|Belbessai, Boualem|300150205|300150205|Student|sept. 8 2025 22 h 01
-| Afficher le profil de Mohand Said Kemiche|Kemiche, Mohand Said  Mohand Said Kemiche est en ligne|300150268|300150268|Student|sept. 9 2025 16 h 44
-| Afficher le profil de Mohand Tahar Aroua|Aroua, Mohand Tahar|300150284|300150284|Student|
-| Afficher le profil de Lounas Allouti|Allouti, Lounas|300150295|300150295|Student|
-| Afficher le profil de Youba Bouanani|Bouanani, Youba  Youba Bouanani est en ligne|300150296|300150296|Student|sept. 8 2025 21 h 50
-| Afficher le profil de Jesmina Ludy Maud Afi Dos-Reis|Dos-Reis, Jesmina Ludy Maud Afi|300150303|300150303|Student|sept. 9 2025 09 h 14
-| Afficher le profil de Belkacem Medjkoune|Medjkoune, Belkacem|300150385|300150385|Student|
-| Afficher le profil de Ismail Trache|Trache, Ismail|300150395|300150395|Student|sept. 4 2025 23 h 41
-| Afficher le profil de Chakib Rahmani|Rahmani, Chakib  Chakib Rahmani est en ligne|300150399|300150399|Student|sept. 9 2025 16 h 37
-| Afficher le profil de Abdelatif Nemous|Nemous, Abdelatif  Abdelatif Nemous est en ligne|300150417|300150417|Student|
-| Afficher le profil de Akrem Bouraoui|Bouraoui, Akrem  Akrem Bouraoui est en ligne|300150527|300150527|Student|
-| Afficher le profil de Hicham Hamdi|Hamdi, Hicham|300151042|300151042|Student|sept. 3 2025 15 h 45
-| Afficher le profil de Syphax Ouadfel|Ouadfel, Syphax  Syphax Ouadfel est en ligne|300151233|300151233|Student|sept. 5 2025 10 h 45
-| Afficher le profil de Amine Kahil|Kahil, Amine  Amine Kahil est en ligne|300151292|300151292|Student|sept. 9 2025 16 h 42
-| Afficher le profil de Mohand L'hacene Hammiche|Hammiche, Mohand L'hacene Mohand L'hacene Hammiche est en ligne|300151492|300151492|Student|sept. 9 2025 16 h 39
-| Afficher le profil de Mohammed Aiche|Aiche, Mohammed  Mohammed Aiche est en ligne|300151608|300151608|Student|sept. 6 2025 10 h 59
-| Afficher le profil de Massinissa Mameri|Mameri, Massinissa|300151841|300151841|Student|
-| Afficher le profil de Imad Boudeuf|Boudeuf, Imad|300152410|300152410|Student|
-| Afficher le profil de Thierno Madjou Bah|Bah, Thierno Madjou  Thierno Madjou Bah est en ligne|300153747|300153747|Student|
+#!/bin/bash
+
+ETUDIANTS=(
+300138205
+300146667
+300150205
+300150268
+300150284
+300150295
+300150296
+300150303
+300150385
+300150395
+300150399
+300150417
+300150527
+300151042
+300151233
+300151292
+300151492
+300151608
+300151841
+300152410
+300153747
+)
+
+IDS=(
+300138205
+300146667
+300150205
+300150268
+300150284
+300150295
+300150296
+300150303
+300150385
+300150395
+300150399
+300150417
+300150527
+300151042
+300151233
+300151292
+300151492
+300151608
+300151841
+300152410
+300153747
+)
+
+AVATARS=(
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+583231
+)
